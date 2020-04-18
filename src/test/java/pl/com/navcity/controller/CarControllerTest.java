fb4cbd824deeed80d9e6412719aeff7c6239b815
Dominik <dominikjaniga91@gmail.com>
2020-04-18 19:14:47 +0200

    Add test to check setting car distance method

--- src/test/java/pl/com/navcity/controller/CarControllerTest.java
+++ src/test/java/pl/com/navcity/controller/CarControllerTest.java
@@ -1,10 +1,14 @@
 package pl.com.navcity.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,9 +18,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import pl.com.navcity.model.Car;
 import pl.com.navcity.model.Color;
+import pl.com.navcity.model.Route;
 import pl.com.navcity.service.CarServiceImpl;
 import pl.com.navcity.service.RouteServiceImpl;
 import pl.com.navcity.service.UserDetailsServiceImpl;
+
+import javax.validation.Valid;
 import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -42,15 +49,12 @@ public class CarControllerTest {
     UserDetailsServiceImpl userDetailsService;
 
 
-    @BeforeEach
-    void setUp(){
-        Car car = new Car("Porsche", "Panamera", "5443123221234", Color.Black, 2011, null);
-        BDDMockito.given(carService.getAllCars()).willReturn(Arrays.asList(car));
-    }
-
     @Test
     @WithMockUser(username = "Dominik", roles = {"ADMIN", "MANAGER"})
     void resultShouldMatchCar_whenConnectToEndpoint() throws Exception {
+
+        Car car = new Car("Porsche", "Panamera", "5443123221234", Color.Black, 2011, null);
+        BDDMockito.given(carService.getAllCars()).willReturn(Arrays.asList(car));
 
         mockMvc.perform(get("/api/cars/list"))
                 .andExpect(status().isOk())
@@ -66,4 +70,29 @@ public class CarControllerTest {
                         ))));
 
     }
+
+    @Test
+    void shouldReturnProperValue_afterAddDistanceToCar(){
+
+        Car car = new Car("Porsche", "Panamera", "5443123221234", Color.Black, 2011, null);
+        Route route = new Route();
+        route.setDistance(103560.00);
+
+        car.setRouteDurationAndDistance(route);
+
+        Assertions.assertEquals(103.56, car.getDistance());
+
+    }
+
+        @Test
+        void shouldReturnDistanceEqualsToZero_afterdeleteRoute(){
+            Car car = new Car("Porsche", "Panamera", "5443123221234", Color.Black, 2011, null);
+            Route route = new Route();
+            car.deleteRouteAndUpdateTimeDistance(route);
+
+            Assertions.assertEquals(0.00, car.getDistance());
+
+        }
+
+
 }
